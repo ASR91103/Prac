c5de6850c4e98ff56e71b1f607c8160820c6cbf6
Shradha Arsewar <146162679+ASR91103@users.noreply.github.com>
2025-01-21 00:51:42 +0530

    Create largestele_1_way.cpp

---
+++ largestele_1_way.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>  // For sort() function
+#include<climits>    // For INT_MIN and INT_MAX
+using namespace std;
+ 
+int sorting(vector<int>& arr) {
+    sort(arr.begin(),arr.end());
+    return arr[arr.size()-1];
+}
+int main() {
+    vector<int> array1;
+    int n,a;
+    cout<<"Enter the size of the array";
+    cin>>n;
+    for(int i=0 ; i<n ; i++)
+    {
+        cin>>a;
+        array1.push_back(a);
+    }
+    cout<<"The largest element in the array is: "<<sorting(array1)<<endl;
+    return 0;
+}
