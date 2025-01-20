ee49666596c45d47fd7e12d0f65544df2ec5c024
Shradha Arsewar <146162679+ASR91103@users.noreply.github.com>
2025-01-21 01:30:45 +0530

    Create median.cpp

---
+++ median.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<vector>
+#include<algorithm> 
+#include<climits>    
+using namespace std;
+
+void Median(int arr[] ,int n)
+{
+    sort(arr,arr +n);
+    if(n%2==0)
+    {
+        int ind1 = (n/2)-1;
+        int ind2 = (n/2);
+        cout<<(double)(arr[ind1]+arr[ind2])/2;
+    }
+    else
+    {
+        cout<<arr[(n/2)];
+    }
+}
+int main(){
+    int arr[] = {4,7,1,2,5,6};
+    int n = 6;
+     cout<<"The median of the array is: "<<Median(array1,n);
+    return 0;
+}
