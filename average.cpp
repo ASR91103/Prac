085210ea3a5eec2e855b667f078d47f07082582c
Shradha Arsewar <146162679+ASR91103@users.noreply.github.com>
2025-01-21 01:25:40 +0530

    Create average.cpp

---
+++ average.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<vector>
+#include<algorithm> 
+#include<climits>    
+using namespace std;
+int main(){
+    vector<int> array1;
+    int n,a;
+    cout<<"Enter the size of the array";
+    cin>>n;
+    for(int i=0 ; i<n ; i++)
+    {
+        cin>>a;
+        array1.push_back(a);
+    }
+    int sum = 0;
+	for (int i = 0; i < n; i++) {
+		sum += (int)array1[i];
+	}
+	cout <<"The sum is: "<<sum;
+    double average = sum / n;
+	cout << "The average is "<<average;
+    return 0;
+}
